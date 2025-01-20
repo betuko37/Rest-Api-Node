aef973a4721c7fabd5637af900e52f0802d2d697
Jesus Zavala <betorolitos37@gmail.com>
2025-01-19 22:49:24 -0700

    sss

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,16 +4,16 @@ const ACCEPTED_ORIGINS = [
   'https://rest-api-node-4ufv.onrender.com' 
 ];
 
-export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => 
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
     origin: (origin, callback) => {
-      // Permitir si el origen está en la lista aceptada
-      if (acceptedOrigins.includes(origin)) {
-        return callback(null, true);
+      // Si el origen está en la lista, o si no se incluye un origen (por ejemplo, herramientas como Postman)
+      if (acceptedOrigins.includes(origin) || !origin) {
+        return callback(null, true); // Permite la solicitud
       }
 
-      
-      // Bloquear cualquier otro origen no permitido
+      // Bloquear otros orígenes
       return callback(new Error('Not allowed by CORS'));
     },
+    methods: ['GET'], // Solo permitir GET
   });
